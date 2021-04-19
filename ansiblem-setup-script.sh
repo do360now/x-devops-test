37f31674255b546a79342e4af8c06052d9e99e71
do360now <58228876+do360now@users.noreply.github.com>
2021-04-19 10:22:37 +0200

    changes

--- ansiblem-setup-script.sh
+++ ansiblem-setup-script.sh
@@ -8,9 +8,3 @@ sudo -H python3 -m pip install ansible
 ansible --version
 ansible-playbook devops-test-deploy.yml -i ./devops-test-inventory/hosts
 #etc.
-
-
-
-# sudo apt update
-# sudo apt install python3-pip
-# sudo python3 -m pip install ansible -y
